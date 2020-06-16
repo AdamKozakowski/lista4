a937db076bb64daafea28b2694bc262e16d930fc
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:22:43 +0200

    Update Triangle.cpp
    Metoda obliczania pola, wykorzystujaca wzór Herona.

--- Zad2/Triangle.cpp
+++ Zad2/Triangle.cpp
@@ -1,9 +1,17 @@
 #include "Triangle.hpp"
 #include "Figure.hpp"
+#include <math.h>
 
 Triangle::Triangle(double a, double b, double c) :
     _first_side (a > 0 ? (a < b + c ? a : throw std::domain_error("Taki trojkat nie istnieje  (a>=b+c)")) : throw std::domain_error("a musi byc >0")),
     _second_side (b > 0 ? (b < a + c ? a : throw std::domain_error("Taki trojkat nie istnieje  (b>=a+c)")) : throw std::domain_error("b musi byc >0")),
     _third_side (c > 0 ? (c < a + b ? a : throw std::domain_error("Taki trojkat nie istnieje (c>=a+b)")) : throw std::domain_error("c musi byc >0")),
     _name_of_figure("Trojkat"){ 
+}
+
+//metoda obliczania pola
+double Triangle::area() const{ 
+    //ze wzoru Herona
+    double p = (_first_side + _second_side + _third_side)/2;
+    return sqrt(p*(p-_first_side)*(p-_second_side)*(p-_third_side)); 
 }
