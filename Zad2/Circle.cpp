19cf5b59200510867a3ca270ce467a36075b5b50
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:17:41 +0200

    Update Circle.cpp
    metoda wyznaczania pola.

--- Zad2/Circle.cpp
+++ Zad2/Circle.cpp
@@ -1,7 +1,14 @@
 #include "Circle.hpp"
 
+double pi = 3.13159;
+
 //konstruktor jednoargumentowy wyposazony w kontrole wartosci promienia
 Circle::Circle(double r) : 
      _radius(r > 0 ? r : throw std::domain_error("Promien ma byc wiekszy od 0!")),
     _name_of_figure("Kolo"){
     };
+
+//metoda obliczania pola
+double Circle::area() const{
+    return pi * _radius * _radius;
+}
