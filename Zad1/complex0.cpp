2de22a78defeb0664db6282042c3d175b66c4554
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:01:10 +0200

    Update complex0.cpp
    Dodanie metody pobierania wartości liczby zespolonej.

--- Zad1/complex0.cpp
+++ Zad1/complex0.cpp
@@ -19,3 +19,11 @@ const Complex0 &Complex0::operator=(const Complex0 &cx){
     _real = cx._real;
     _imag = cx._imag;
 }
+
+//metoda do pobierania wartosci rzeczywistych i urojonych liczby zespolonej
+double Complex0::getReal() const {
+    return _real;
+}
+double Complex0::getImag() const {
+    return _imag;
+}
