93d5a44ed5776cbe8ecb8a7681883a7012666507
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:30:53 +0200

    Update Rectangle.cpp
    Metoda obliczania pola.

--- Zad2/Rectangle.cpp
+++ Zad2/Rectangle.cpp
@@ -1,8 +1,14 @@
 #include "Rectangle.hpp"
 #include <iostream>
 
+//konstruktor dwuargumentowy z kontrola danych
 Rectangle::Rectangle(const double a, const double b) : 
     _first_side(a > 0 ? a: throw std::domain_error("Bok musi byc wyrazony liczba dodatnia")),
     _secod_side(b > 0 ? b: throw std::domain_error("Bok musi byc wyrazony liczba dodatnia")),
     _name_of_figure("Prostokat"){
 };
+
+//metoda pola
+double Rectangle::area () const {
+    return _first_side * _secod_side;
+}
