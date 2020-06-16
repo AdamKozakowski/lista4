e1d3672899e7fef6f9c98002b01f20c82b52d17b
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 11:59:06 +0200

    Update complex0.cpp
    Dodanie drugiego konstruktora.

--- Zad1/complex0.cpp
+++ Zad1/complex0.cpp
@@ -3,3 +3,7 @@
 //konstruktor domyślny
 Complex0::Complex0() : _real(0), _imag(0){
 }
+
+//konstruktor dla dwoch argumentów
+Complex0::Complex0(double real, double imag) : _real(real), _imag(imag){
+}
