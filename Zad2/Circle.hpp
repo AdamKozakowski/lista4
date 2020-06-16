1f5c50ec998b3fbb15439c70abecbb5d8fec7e56
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:14:05 +0200

    Update Circle.hpp
    Stworzenie interfejsu klasy circle.

--- Zad2/Circle.hpp
+++ Zad2/Circle.hpp
@@ -1,4 +1,18 @@
 #ifndef CIRCLE_HPP_INCLUDED
 #define CIRCLE_HPP_INCLUDED
 
+#include <iostream>
+#include "Figure.hpp"
+#include <string>
+
+class Circle : public Figure{
+    public: 
+        Circle (double r);
+        double area() const;
+        std::ostream& print(std::ostream&) const; 
+    private:
+        double _radius;
+        std::string _name_of_figure;
+};
+
 #endif
