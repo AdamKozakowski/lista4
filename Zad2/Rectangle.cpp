b2eae3ea19e76588b4dba7f42121c06d4fe344d9
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:28:42 +0200

    Update Rectangle.cpp
    Dodanie konstruktora dwuargumentowego z kontrolą danych.

--- Zad2/Rectangle.cpp
+++ Zad2/Rectangle.cpp
@@ -1,1 +1,8 @@
 #include "Rectangle.hpp"
+#include <iostream>
+
+Rectangle::Rectangle(const double a, const double b) : 
+    _first_side(a > 0 ? a: throw std::domain_error("Bok musi byc wyrazony liczba dodatnia")),
+    _secod_side(b > 0 ? b: throw std::domain_error("Bok musi byc wyrazony liczba dodatnia")),
+    _name_of_figure("Prostokat"){
+};
