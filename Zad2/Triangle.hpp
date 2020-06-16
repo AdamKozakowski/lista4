6082620328a1863755ab951d47eff05206f0dccb
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:19:52 +0200

    Update Triangle.hpp
    Stworzenie interfejsu klasy Triangle.

--- Zad2/Triangle.hpp
+++ Zad2/Triangle.hpp
@@ -1,4 +1,20 @@
 #ifndef TRIANGLE_HPP_INCLUDED
 #define TRIANGLE_HPP_INCLUDED
 
+#include "Figure.hpp"
+#include <iostream>
+#include <string>
+
+class Triangle : public Figure{
+    public:
+        Triangle (double a, double b, double c);
+        double area () const;
+        std::ostream& print (std::ostream &) const;
+    private:
+        double _first_side;
+        double _second_side;
+        double _third_side;
+        std::string _name_of_figure;
+};
+
 #endif
