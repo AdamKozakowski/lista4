6cacfbe4e6ddc2d0883002107cbe51df80cddf15
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:24:29 +0200

    Update Rectangle.hpp
    Stworzenie interfejsu klasy Rectangle.

--- Zad2/Rectangle.hpp
+++ Zad2/Rectangle.hpp
@@ -1,5 +1,22 @@
 #ifndef RECTANGLE_HPP_INCLUDED
 #define RECTANGLE_HPP_INCLUDED
 
+#include "Figure.hpp"
+#include <iostream>
+#include <string>
+
+class Rectangle : public Figure {
+    public:
+        //konstruktor dwuargumentowy 
+        Rectangle(const double a, const double b);
+        //metoda obliczania pola figury
+        double area () const;
+        //metoda wypisywania
+        std::ostream &print(std::ostream &) const;
+    private: 
+        double _first_side;
+        double _secod_side;
+        std::string _name_of_figure;
+};
 
 #endif
