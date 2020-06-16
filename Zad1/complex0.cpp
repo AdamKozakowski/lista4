a2b5ede4e48c1e916c270e561e39fc261cc7f70b
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 11:59:33 +0200

    Update complex0.cpp
    Dodanie konstruktora kopiujacego.

--- Zad1/complex0.cpp
+++ Zad1/complex0.cpp
@@ -7,3 +7,9 @@ Complex0::Complex0() : _real(0), _imag(0){
 //konstruktor dla dwoch argumentów
 Complex0::Complex0(double real, double imag) : _real(real), _imag(imag){
 }
+
+//konstruktor kopiujący
+Complex0::Complex0(const Complex0 &cx){
+    _real = cx._real;
+    _imag = cx._imag;
+}
