352774ad3c3007a6c73318f567e9efe86e158872
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:16:43 +0200

    Update Circle.cpp
    Konstruktor.

--- Zad2/Circle.cpp
+++ Zad2/Circle.cpp
@@ -1,1 +1,7 @@
 #include "Circle.hpp"
+
+//konstruktor jednoargumentowy wyposazony w kontrole wartosci promienia
+Circle::Circle(double r) : 
+     _radius(r > 0 ? r : throw std::domain_error("Promien ma byc wiekszy od 0!")),
+    _name_of_figure("Kolo"){
+    };
