82492f02f65759f1ad9b125290c694b93a111d55
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-16 12:11:35 +0200

    Update Figure.hpp
    Utworzenie interfejsu klasy Figure zgodnie z instrukcją do zadania.

--- Zad2/Figure.hpp
+++ Zad2/Figure.hpp
@@ -1,4 +1,15 @@
 #ifndef FIGURE_HPP_INCLUDED
 #define FIGURE_HPP_INCLUDED
 
+#include <iostream>
+
+class Figure{
+    public:
+        virtual double area() const = 0;
+        virtual std::ostream& print(std::ostream&) const = 0;
+};
+
+//przeciazony operator do wyswietlania
+std::ostream& operator<<(std::ostream& out, Figure& f);
+
 #endif
