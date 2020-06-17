a6112e17dd317a84fcf6febe07b155b68eb4995e
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-17 22:46:50 +0200

    Update ksiazka_adresowa.cpp
    Zdefiniowanie funkcji insert.

--- Zad3/ksiazka_adresowa.cpp
+++ Zad3/ksiazka_adresowa.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 //Funkcja wyswietlajaca mozliwe komendy
-void e_help()
-{
+void e_help(){
     cout << "Ksiazka adresowa Obslugiwane komendy:\n"
          << "\t HELP \t wyswietla te pomoc\n"
          << "\t INSERT  dodaje element do ksiazki adresowej (INSERT adam adamkozak@smail.com)\n"
@@ -15,4 +14,25 @@ void e_help()
          << "\t CLEAR \t usuwa WSZYSTKIE elementy z ksiazki adresowej\n"
          << "\t PRINT \t wypisuje na ekran wszystkie elementy ksiazki adresowej\n"
          << "\t EXIT \t konczy prace programu (program zapisze ksiazke do pliku \"ksiazka_adresowa.csv\", ktory odczyta przy kolejnym uruchomieniu)\n\n";
+}
+
+bool e_insert(map<string, string> &ebook){
+    string name, email;
+    cin >> name >> email;
+    size_t at = email.find_first_of('@');
+    size_t dot = email.find_last_of('.');
+    if (at != string::npos && at < dot){
+        ebook.insert({name, email});
+        if (ebook[name] != email){
+            cout << "Nie udalo sie dodac rekordu, prawdopodobnie w bazie znajduje sie juz klucz '"
+                 << name << "', sprobuj uzyc komendy EMPLACE.\n";
+            return false;
+        }
+        cout << "Pomyslnie dodano " << name << " : " << ebook[name] << "\n";
+        return true;
+    }
+    else{
+        cout << "Sprobuj ponownie podajac prawidlowy email (np. danhouser@email.com)\n";
+        return false;
+    }
 }
