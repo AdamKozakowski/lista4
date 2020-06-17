77c7134685c58d84a8b9dd37db6005a5c6df1be1
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-17 22:39:15 +0200

    Update ksiazka_adresowa.hpp
    Zdeklarowanie potrzebnych funkcji.

--- Zad3/ksiazka_adresowa.hpp
+++ Zad3/ksiazka_adresowa.hpp
@@ -1,6 +1,31 @@
 #ifndef KSIAZKA_ADRESOWA_HPP_INCLUDED
 #define SIAZKA_ADRESOWA_HPP_INCLUDED
 
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+//Funkcja wyswietlajaca mozliwe komendy
+void e_help();
+
+//funckja 
+bool e_insert(std::map<std::string, std::string> &ebook);
+
+//funkcja zwracajaca wartosc klucza
+bool e_at(std::map<std::string, std::string> &ebook);
+
+//funckja zmieniajaca wartosc klucza
+bool e_emplace(std::map<std::string, std::string> &ebook);
+
+//funkcja usuwajaca klucz
+bool e_erase(std::map<std::string, std::string> &ebook);
+
+//funkcja usuwajaca ksiazke
+bool e_clear(std::map<std::string, std::string> &ebook);
+
+//funckja wyswietlania
+bool e_print(std::map<string, std::string> &ebook);
 
 
 #endif
