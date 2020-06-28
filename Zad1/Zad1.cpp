6cf118e7dfe8fe06a617888488622bdf44c173e9
AdamKozakowski <62246554+AdamKozakowski@users.noreply.github.com>
2020-06-28 18:03:15 +0200

    Update Zad1.cpp
    Zaimplementowanie dodanych przeciazonych operatorow.

--- Zad1/Zad1.cpp
+++ Zad1/Zad1.cpp
@@ -13,13 +13,15 @@ int main(){
     c0 = c1;
     std::cout << "Przeciazony operator przypisywania = (c0=c1): " << c0 << "\n"; 
     std::cout << "Dodawanie dwoch liczb zespolonych: (c0+c1): " << c0+c1 << "\n";
-    //Liczba rzeczywista to zespolona z zerowa wartoscia urojona, wiec metody wykorzystujace przeciazone operatory spelniaja warunki dla liczb rzeczywistych przedstawionych w ten sposob. Przedstawienie liczb rzeczywistych przy pomocy np. typu double wprowadza wiele komplikacji np. niezgodnosc typow podczas operacji dodawania.
-    Complex0 r1(5,0);
+    double r1 = 5;
     std::cout << "Liczba rzeczywista r1: " << r1 << "\n";
-    std::cout << "Dodawanie liczby rzeczywistej do zespolonej: (r1+c1): " << r1+c1 << "\n";
+    std::cout << "Dodawanie liczby rzeczywistej do zespolonej: (c1+r1): " << c1+r1 << "\n";
+    std::cout << "Dodawanie liczby zespolonej do rzeczywistej: (r1+c1): " << r1+c1 << "\n";
     std::cout << "Odejmowanie dwoch liczb zespolonych: (c0-c1): " << c0 - c1 << "\n";
-    std::cout << "Odejmowanie liczby zespolonej od rzeczywistej: (r1-c1): " << r1-c1 << "\n";
+    std::cout << "Odejmowanie liczby rzeczywistej od zespolonej : (c1-r1): " << c1-r1 << "\n";
+    std::cout << "Odejmowanie liczby zespolonej od rzeczywistej : (r1-c1): " << r1-c1 << "\n";
     std::cout << "Mnozenie liczb zespolonych (c1*c2): " << c1*c2 << "\n";
     std::cout << "Mnozenie liczby zespolonej przez rzeczywista (c1 * r1): " << c1*r1 << "\n";
+    std::cout << "Mnozenie liczby rzeczywistej przez zespolona (r1 * c1): " << r1*c1 << "\n";
     std::cout << "Sprzezenie liczby zespolonej (c1): " << sprzezenie(c1) << "\n";
 }
